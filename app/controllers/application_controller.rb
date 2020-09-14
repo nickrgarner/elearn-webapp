eba422dbbfb80d5e98fe46616d87ff0af5f71c87
Ryan Grainger <rpgraing@ncsu.edu>
2020-09-14 13:56:08 -0400

    fixed an issue where teachers and students could enter some urls that should only be accessible to the admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :admin?
   
     def current_user
       if session[:user_id]
@@ -20,5 +21,11 @@ class ApplicationController < ActionController::Base
     	flash[:alert] = "Please Log in"
     	redirect_to root_url unless logged_in?
     end
+
+    def admin?
+    	flash[:notice] = "Page Restricted"
+    	redirect_to home_path unless current_user.userable_type.to_str == "Admin"
+    end
+
   end
   
