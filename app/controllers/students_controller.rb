93899204c35ef09495b0ded613b44c49c1f88fcb
Ryan Grainger <rpgraing@ncsu.edu>
2020-09-18 00:55:04 -0400

    one last attempt to fix signup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,8 @@ class StudentsController < ApplicationController
   before_action :student_edit_access?, only: [:edit, :update]
   before_action :student_show_access?, only: [:show]
   before_action :student_index_access?, only: [:index]
-  before_action :admin?, only: [:new, :create, :destroy]
+  before_action :student_create_access?, only: [:new,:create]
+  before_action :admin?, only: [:destroy]
 
   # GET /students
   # GET /students.json
@@ -103,6 +104,13 @@ class StudentsController < ApplicationController
 
     def student_index_access?
       if current_user.userable_type.to_str != "Admin"
+        flash[:notice] = "Page Restricted"
+        redirect_to home_path
+      end
+    end
+    def student_create_access?
+      if current_user.nil?
+      elsif current_user.userable_type.to_str != "Admin"
         flash[:notice] = "Page Restricted"
         redirect_to home_path
       end
