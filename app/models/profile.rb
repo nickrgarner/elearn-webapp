1426bc2b5acbd0d6dcf1c2b84f23f85f55db8002
Nick Garner <nrgarner@ncsu.edu>
2020-09-19 14:08:29 -0400

    Added phone number regex to profile.rb, updated seeds

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,8 @@ class Profile < ApplicationRecord
     validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email"}
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :phone_number, presence: true
+    validates :phone_number, presence: true, format: {with: /\A[0-9]{3}(?:-)?[0-9]{3}(?:-)?[0-9]{4}\z/, message:
+        "Invalid phone number"}
     validates :address, presence: true
 
     def name
