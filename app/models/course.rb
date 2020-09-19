cf38aea6324d0fb75ab5fdef8bb75f9350f4430e
Nick Garner <nrgarner@ncsu.edu>
2020-09-19 10:44:22 -0400

    Changed course.course_number uniqueness scope

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
     belongs_to :discipline
     has_many :course_sections
-    validates :course_number, numericality: { only_integer: true }
+    validates :course_number, uniqueness: {scope: :discipline_id, message: "Course has already been created."},
+              numericality: { only_integer: true }
     validates :name, presence: true
     validates :area, presence: true
     validates :price, numericality: true
