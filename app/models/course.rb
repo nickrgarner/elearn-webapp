0ebac504005be5303f7c63bcfd2189cd2f67bb55
Nick Garner <nrgarner@ncsu.edu>
2020-09-18 21:54:04 -0400

    Removed uniqueness validation from course_number

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     belongs_to :discipline
     has_many :course_sections
-    validates :course_number, uniqueness: true, numericality: { only_integer: true }
+    validates :course_number, numericality: { only_integer: true }
     validates :name, presence: true
     validates :area, presence: true
     validates :price, numericality: true
