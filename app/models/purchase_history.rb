b5b889633c1abee9fbef42f609361b0a122c808b
Sidharth <smehta22@ncsu.edu>
2020-09-20 15:58:00 -0400

    add to cart enhancements

--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -2,4 +2,5 @@ class PurchaseHistory < ApplicationRecord
     belongs_to :course_section
     belongs_to :student
     has_one :teacher, through: :course_section
+    has_one :course, through: :course_section
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,42 +15,42 @@ d6 = Discipline.create(name: "PHY")
 d7 = Discipline.create(name: "MAT")
 
 s1 = Student.create(discipline_id: 1)
-Profile.create(first_name: "student1", last_name: "GGM", email: "student1@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: s1.id, userable_type: "Student")
+Profile.create(first_name: "student1", last_name: "GGM", email: "student1@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: s1.id, userable_type: "Student")
 Cart.create(student_id: s1.id)
 
 s2 = Student.create(discipline_id: 2)
-Profile.create(first_name: "student2", last_name: "GGM", email: "student2@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: s2.id, userable_type: "Student")
+Profile.create(first_name: "student2", last_name: "GGM", email: "student2@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: s2.id, userable_type: "Student")
 Cart.create(student_id: s2.id)
 
 s3 = Student.create(discipline_id: 1)
-Profile.create(first_name: "student3", last_name: "GGM", email: "student3@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: s3.id, userable_type: "Student")
+Profile.create(first_name: "student3", last_name: "GGM", email: "student3@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: s3.id, userable_type: "Student")
 Cart.create(student_id: s3.id)
 
 s4 = Student.create(discipline_id: 2)
-Profile.create(first_name: "student4", last_name: "GGM", email: "student4@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: s4.id, userable_type: "Student")
+Profile.create(first_name: "student4", last_name: "GGM", email: "student4@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: s4.id, userable_type: "Student")
 Cart.create(student_id: s4.id)
 
 s5 = Student.create(discipline_id: 4)
-Profile.create(first_name: "student5", last_name: "GGM", email: "student5@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: s5.id, userable_type: "Student")
+Profile.create(first_name: "student5", last_name: "GGM", email: "student5@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: s5.id, userable_type: "Student")
 Cart.create(student_id: s5.id)
 
 a = Admin.create()
-Profile.create(first_name: "admin", last_name: "GGM", email: "admin@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: a.id, userable_type: "Admin")
+Profile.create(first_name: "admin", last_name: "GGM", email: "admin@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: a.id, userable_type: "Admin")
 
 t1 = Teacher.create(discipline_id: 1)
-Profile.create(first_name: "teacher1", last_name: "GGM", email: "teacher1@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: t1.id, userable_type: "Teacher")
+Profile.create(first_name: "teacher1", last_name: "GGM", email: "teacher1@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: t1.id, userable_type: "Teacher")
 
 t2 = Teacher.create(discipline_id: 2)
-Profile.create(first_name: "teacher2", last_name: "GGM", email: "teacher2@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: t2.id, userable_type: "Teacher")
+Profile.create(first_name: "teacher2", last_name: "GGM", email: "teacher2@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: t2.id, userable_type: "Teacher")
 
 t3 = Teacher.create(discipline_id: 2)
-Profile.create(first_name: "teacher3", last_name: "GGM", email: "teacher3@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: t3.id, userable_type: "Teacher")
+Profile.create(first_name: "teacher3", last_name: "GGM", email: "teacher3@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: t3.id, userable_type: "Teacher")
 
 t4 = Teacher.create(discipline_id: 1)
-Profile.create(first_name: "teacher4", last_name: "GGM", email: "teacher4@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: t4.id, userable_type: "Teacher")
+Profile.create(first_name: "teacher4", last_name: "GGM", email: "teacher4@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: t4.id, userable_type: "Teacher")
 
 t5 = Teacher.create(discipline_id: 3)
-Profile.create(first_name: "teacher5", last_name: "GGM", email: "teacher5@GGM.edu", phone_number: 999-999-9999, address: "internet", password: "123", password_confirmation: "123", userable_id: t5.id, userable_type: "Teacher")
+Profile.create(first_name: "teacher5", last_name: "GGM", email: "teacher5@GGM.edu", phone_number: 9999999999, address: "internet", password: "123", password_confirmation: "123", userable_id: t5.id, userable_type: "Teacher")
 
 c1 = Course.create(course_number: 517, name: "Object Oriented Design Development", discipline_id: 1, area: "Software Development", price: 999)
 c2 = Course.create(course_number: 563, name: "Micro Architecture", discipline_id: 2, area: "Computer Architecture", price: 999)
