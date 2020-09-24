26cfddc327a53c4fe781d8dbb144d30f76a5d1ea
Nick Garner <nrgarner@ncsu.edu>
2020-09-24 13:59:13 -0400

    Created CRUD spec tests for courses_controller

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CoursesController do
     before do
         @user = FactoryBot.create(:profile)
         @request.session[:user_id] = @user.id
-        create(:course)
+        @course = create(:course)
     end
     
     describe "Students" do
@@ -13,6 +13,25 @@ RSpec.describe CoursesController do
             get :index
             expect(response).to render_template(:index)
             expect(response).to have_http_status(:success)
+            expect(assigns(:courses)).to_not be_nil
+        end
+        it "should be able to show an individual course" do
+          get :show, params: {id: @course.id}
+          expect(response).to render_template(:show)
+          expect(response).to have_http_status(:success)
+        end
+        it "should not be able to create a course" do
+          get :new
+          expect(response).to redirect_to(home_path)
+          expect(assigns(:course)).to be_nil
+        end
+        it "should not be able to edit a course" do
+          get :edit, params: {id: @course.id}
+          expect(response).to redirect_to(home_path)
+        end
+        it "should not be able to destroy a course" do
+          get :destroy, params: {id: @course.id}
+          expect(response).to redirect_to(home_path)
         end
     end
 end
